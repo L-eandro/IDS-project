bf5d1f551eb5b3fb468cdf8404091f25a8c5018f
Rafael Almeida <rafael_solos@outlook.com>
2024-09-11 23:03:52 -0300

    Update
    Final

--- code/main.py
+++ code/main.py
@@ -9,18 +9,17 @@ from monitor_ports import collect_port_data
 from intrusion_detect import simulate_intrusion_detection
 from geomap import simulate_attacks
 from scan_ports import scan_vulnerabilities, start_nmap_prometheus_server
+from teste_nessus import update_prometheus_metrics
 
 import threading
 import time
 
 def start_attack_simulation():
-
     attack_thread = threading.Thread(target=simulate_attacks)
     attack_thread.daemon = True
     attack_thread.start()
 
 def run_background_tasks():
-
     while True:
         fetch_external_data()
         collect_port_data()
@@ -28,30 +27,28 @@ def run_background_tasks():
         simulate_intrusion_detection()
         time.sleep(60)
 
-def main():
+def run_nessus_analysis():
+    nessus_file_path = 'F:/Projeto-IDS/IDS-project/IDS-project/Others/Varredura.nessus'
+    while True:
+        update_prometheus_metrics(nessus_file_path)
+        time.sleep(300)
 
+def main():
     start_http_server(8000)
     print("Servidor Prometheus rodando na porta 8000...")
 
-
     start_attack_simulation()
-
-
     start_nmap_prometheus_server()
-
 
     historical_data = collect_historical_data()
     if not historical_data.empty:
         analyze_and_predict(historical_data)
 
-
     check_server()
     update_execution_time()
 
-
     monitor_network()
     collect_port_data()
-
 
     target = '192.168.18.1'
     scan_vulnerabilities(target)
@@ -59,6 +56,10 @@ def main():
     start_packet_sniffing()
     simulate_intrusion_detection()
 
+
+    nessus_thread = threading.Thread(target=run_nessus_analysis)
+    nessus_thread.daemon = True
+    nessus_thread.start()
 
     run_background_tasks()
 
--- code/teste_nessus.py
+++ code/teste_nessus.py
@@ -2,32 +2,32 @@ import xml.etree.ElementTree as ET
 from prometheus_client import start_http_server, Gauge
 import time
 
-# Define métricas Prometheus para cada nível de severidade
+
 low_vuln_gauge = Gauge('vulnerabilities_low', 'Low severity vulnerabilities detected')
 medium_vuln_gauge = Gauge('vulnerabilities_medium', 'Medium severity vulnerabilities detected')
 high_vuln_gauge = Gauge('vulnerabilities_high', 'High severity vulnerabilities detected')
 critical_vuln_gauge = Gauge('vulnerabilities_critical', 'Critical severity vulnerabilities detected')
 
 
-# Função para analisar vulnerabilidades a partir de um arquivo .nessus (XML)
+
 def analyze_nessus_file(file_path):
-    # Carrega o arquivo .nessus (XML)
+
     tree = ET.parse(file_path)
     root = tree.getroot()
 
-    # Inicializa contadores de vulnerabilidades por severidade
+
     low_vulns = 0
     medium_vulns = 0
     high_vulns = 0
     critical_vulns = 0
 
-    # Itera sobre os resultados das vulnerabilidades
+
     for report in root.iter('ReportItem'):
         severity = int(report.get('severity'))
 
-        # Atribui severidade de acordo com os níveis
+
         if severity == 0:
-            continue  # Ignora vulnerabilidades informativas
+            continue
         elif severity == 1:
             low_vulns += 1
         elif severity == 2:
@@ -37,11 +37,11 @@ def analyze_nessus_file(file_path):
         elif severity == 4:
             critical_vulns += 1
 
-    # Retorna o total de vulnerabilidades categorizadas por severidade
+
     return low_vulns, medium_vulns, high_vulns, critical_vulns
 
 
-# Função para atualizar as métricas do Prometheus
+
 def update_prometheus_metrics(file_path):
     low, medium, high, critical = analyze_nessus_file(file_path)
 
@@ -53,17 +53,17 @@ def update_prometheus_metrics(file_path):
 
 
 def main():
-    # Inicia o servidor HTTP para o Prometheus na porta 8000
+
     start_http_server(8000)
     print("Servidor Prometheus rodando na porta 8000...")
 
-    # Arquivo Nessus exportado (modifique o caminho conforme necessário)
-    nessus_file_path = 'C:\\Users\\João\\Documents\\MeusProjetos\\projeto\\IDS-project\\Others\\Varredura.nessus'
 
-    # Atualiza as métricas periodicamente
+    nessus_file_path = 'F:/Projeto-IDS/IDS-project/IDS-project/Others/Varredura.nessus'
+
+
     while True:
         update_prometheus_metrics(nessus_file_path)
-        time.sleep(300)  # Atualiza as métricas a cada 5 minutos
+        time.sleep(300)
 
 
 if __name__ == "__main__":
